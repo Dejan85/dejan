d6845315da9b449e559c6ecca0f5bb7126f4219d
Dejan85 <xypnotica@gmail.com>
2019-12-19 14:49:54 +0100

    Updates

---
+++ precache-manifest.4f12fc66d0044cf8e56a865ec1beda7e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "356401c2ae03dc992a511f3d768d88fd",
+    "url": "/dejan/index.html"
+  },
+  {
+    "revision": "271598b947e1d23f6b5b",
+    "url": "/dejan/static/css/main.25605931.chunk.css"
+  },
+  {
+    "revision": "0f8aae62d565201e7e33",
+    "url": "/dejan/static/js/2.15ec75ee.chunk.js"
+  },
+  {
+    "revision": "783efe56c297789f679b639c509e9a30",
+    "url": "/dejan/static/js/2.15ec75ee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "271598b947e1d23f6b5b",
+    "url": "/dejan/static/js/main.98c29aa6.chunk.js"
+  },
+  {
+    "revision": "902ace99a55763593c09",
+    "url": "/dejan/static/js/runtime-main.b42d494c.js"
+  },
+  {
+    "revision": "edf45df5b73ffd95b30e7a5460a551e1",
+    "url": "/dejan/static/media/luckgame12.edf45df5.jpg"
+  },
+  {
+    "revision": "d67c9fba4785e72b1b2cec6e434edcc3",
+    "url": "/dejan/static/media/porfolio1.d67c9fba.jpg"
+  }
+]);
