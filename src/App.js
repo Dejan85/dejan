7d38922858edfaa7a0c82e731a13a4a57d776229
Dejan85 <xypnotica@gmail.com>
2019-12-19 15:42:17 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,66 +1,64 @@
-import React from 'react';
-import './sass/App.scss';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import "./sass/App.scss";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // components
-import Header from './components/layout/header/Header';
-import Home from './components/layout/home/Home';
-import About from './components/layout/about/About';
-import Skills from './components/layout/skills/Skills';
-import Links from './components/layout/links/Links';
-import Projects from './components/layout/projects/Projects';
-import ProjectsPage from './components/layout/projects/ProjectsPage';
-import Blog from './components/layout/blog/Blog';
+import Header from "./components/layout/header/Header";
+import Home from "./components/layout/home/Home";
+import About from "./components/layout/about/About";
+import Skills from "./components/layout/skills/Skills";
+import Links from "./components/layout/links/Links";
+import Projects from "./components/layout/projects/Projects";
+import ProjectsPage from "./components/layout/projects/ProjectsPage";
+import Blog from "./components/layout/blog/Blog";
 // import Contact from './components/layout/contact/Contact';
-import Footer from './components/layout/footer/Footer';
-import SinglePost from './components/layout/singlePost/SignlePost';
+import Footer from "./components/layout/footer/Footer";
+import SinglePost from "./components/layout/singlePost/SignlePost";
 
 // demo
-import ColorGame from './components/demo/colorGame/ColorGame';
+import ColorGame from "./components/demo/colorGame/ColorGame";
 
 //utils
-import ContentEdit from './components/layout/util/ContentEdit';
-
-
+import ContentEdit from "./components/layout/util/ContentEdit";
 
 //redux
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
-	return (
-		<div className="App">
-			<Provider store={store}>
-				<Router>
-					<Header />
-					<Switch>
-						<Route exact path="/">
-							<Home />
-							<About />
-							<Links />
-							<Skills />
-							<Projects />
-							<Blog />
-							{/* <Contact /> */}
-						</Route>
-						<Route exact path="/projects/:name">
-							<SinglePost />
-						</Route>
-						<Route exact path="/projects">
-							<ProjectsPage />
-						</Route>
-						<Route exact path="/demo/:name">
-							<ColorGame />
-						</Route>
-						<Route exact path="/editcode">
-							<ContentEdit />
-						</Route>
-					</Switch>
-					<Footer />
-				</Router>
-			</Provider>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Provider store={store}>
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path="/portfolio">
+              <Home />
+              <About />
+              <Links />
+              <Skills />
+              <Projects />
+              <Blog />
+              {/* <Contact /> */}
+            </Route>
+            <Route exact path="/projects/:name">
+              <SinglePost />
+            </Route>
+            <Route exact path="/projects">
+              <ProjectsPage />
+            </Route>
+            <Route exact path="/demo/:name">
+              <ColorGame />
+            </Route>
+            <Route exact path="/editcode">
+              <ContentEdit />
+            </Route>
+          </Switch>
+          <Footer />
+        </Router>
+      </Provider>
+    </div>
+  );
 }
 
 export default App;
--- src/components/layout/header/Nav.js
+++ src/components/layout/header/Nav.js
@@ -1,82 +1,84 @@
-import React, { useState } from 'react';
-import { Link } from 'react-scroll';
-import PropTypes from 'prop-types';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-scroll";
+import PropTypes from "prop-types";
+import { useHistory } from "react-router-dom";
 
 const Nav = ({ changeStyle }) => {
-	const { duration } = useState(500);
-	let history = useHistory();
+  const { duration } = useState(500);
+  let history = useHistory();
 
-	return (
-		<React.Fragment>
-			{(history.location.pathname === '/' && (
-				<ul className="header__ul">
-					<li className="header__li" style={changeStyle}>
-						<Link
-							className="header__link"
-							activeClass="active"
-							to="home"
-							spy={true}
-							smooth={'true'}
-							duration={duration}
-							offset={-150}
-						>
-							Home
-						</Link>
-					</li>
-					<li className="header__li" style={changeStyle}>
-						<Link
-							className="header__link"
-							activeClass="active"
-							to="about"
-							spy={true}
-							smooth={'true'}
-							duration={duration}
-							offset={-150}
-						>
-							About
-						</Link>
-					</li>
-					<li className="header__li" style={changeStyle}>
-						<Link
-							className="header__link"
-							activeClass="active"
-							to="skills"
-							spy={true}
-							smooth={'true'}
-							duration={duration}
-							offset={-150}
-						>
-							Skills
-						</Link>
-					</li>
-					<li className="header__li" style={changeStyle}>
-						<Link
-							className="header__link"
-							activeClass="active"
-							to="projects"
-							spy={true}
-							smooth={'true'}
-							duration={duration}
-							offset={-150}
-						>
-							Projects
-						</Link>
-					</li>
-					<li className="header__li" style={changeStyle}>
-						<Link
-							className="header__link"
-							activeClass="active"
-							to="blog"
-							spy={true}
-							smooth={'true'}
-							duration={duration}
-							offset={-150}
-						>
-							Blog
-						</Link>
-					</li>
-					{/* <li className="header__li" style={changeStyle}>
+  console.log(history.location.pathname);
+
+  return (
+    <React.Fragment>
+      {(history.location.pathname === "portfolio" && (
+        <ul className="header__ul">
+          <li className="header__li" style={changeStyle}>
+            <Link
+              className="header__link"
+              activeClass="active"
+              to="home"
+              spy={true}
+              smooth={"true"}
+              duration={duration}
+              offset={-150}
+            >
+              Home
+            </Link>
+          </li>
+          <li className="header__li" style={changeStyle}>
+            <Link
+              className="header__link"
+              activeClass="active"
+              to="about"
+              spy={true}
+              smooth={"true"}
+              duration={duration}
+              offset={-150}
+            >
+              About
+            </Link>
+          </li>
+          <li className="header__li" style={changeStyle}>
+            <Link
+              className="header__link"
+              activeClass="active"
+              to="skills"
+              spy={true}
+              smooth={"true"}
+              duration={duration}
+              offset={-150}
+            >
+              Skills
+            </Link>
+          </li>
+          <li className="header__li" style={changeStyle}>
+            <Link
+              className="header__link"
+              activeClass="active"
+              to="projects"
+              spy={true}
+              smooth={"true"}
+              duration={duration}
+              offset={-150}
+            >
+              Projects
+            </Link>
+          </li>
+          <li className="header__li" style={changeStyle}>
+            <Link
+              className="header__link"
+              activeClass="active"
+              to="blog"
+              spy={true}
+              smooth={"true"}
+              duration={duration}
+              offset={-150}
+            >
+              Blog
+            </Link>
+          </li>
+          {/* <li className="header__li" style={changeStyle}>
 						<Link
 							className="header__link"
 							activeClass="active"
@@ -89,22 +91,22 @@ const Nav = ({ changeStyle }) => {
 							Contact
 						</Link>
 					</li> */}
-				</ul>
-			)) || (
-				<ul className="header__ul">
-					<li className="header__li" style={changeStyle}>
-						<p className="header__link" onClick={history.goBack}>
-							Go Back
-						</p>
-					</li>
-				</ul>
-			)}
-		</React.Fragment>
-	);
+        </ul>
+      )) || (
+        <ul className="header__ul">
+          <li className="header__li" style={changeStyle}>
+            <p className="header__link" onClick={history.goBack}>
+              Go Back
+            </p>
+          </li>
+        </ul>
+      )}
+    </React.Fragment>
+  );
 };
 
 Nav.propTypes = {
-	changeStyle: PropTypes.object
+  changeStyle: PropTypes.object
 };
 
 export default Nav;
