7f37e813d5b84f2dd7630448b205b033fe175ef4
Dejan85 <xypnotica@gmail.com>
2019-12-28 04:29:18 +0100

    Updates

---
+++ precache-manifest.9d30cc12bc957ab000020d9ff5da845b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "694b0a3ce2261dcc5ec492848ec2c59b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "636c2b2f582d56fadd87",
+    "url": "/portfolio/static/css/main.3ef87623.chunk.css"
+  },
+  {
+    "revision": "4aedec4c3c96390fdbbe",
+    "url": "/portfolio/static/js/2.8fc1aeba.chunk.js"
+  },
+  {
+    "revision": "783efe56c297789f679b639c509e9a30",
+    "url": "/portfolio/static/js/2.8fc1aeba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "636c2b2f582d56fadd87",
+    "url": "/portfolio/static/js/main.a98985a8.chunk.js"
+  },
+  {
+    "revision": "16c109c584451046bd84",
+    "url": "/portfolio/static/js/runtime-main.860ade9b.js"
+  },
+  {
+    "revision": "0f5451f114a4db18603d4a4d65cd1a46",
+    "url": "/portfolio/static/media/5.0f5451f1.jpg"
+  },
+  {
+    "revision": "d67c9fba4785e72b1b2cec6e434edcc3",
+    "url": "/portfolio/static/media/porfolio1.d67c9fba.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.d900dd2d15d249f26f8bce2f91b52ba4.js"
+  "/portfolio/precache-manifest.9d30cc12bc957ab000020d9ff5da845b.js"
 );
 
 self.addEventListener('message', (event) => {
